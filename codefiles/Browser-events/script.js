88176845b86dc3db7fd2a2c7cb6141f7db0efdb8
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-05 20:53:25 +0530

    browser events

--- codefiles/Browser-events/script.js
+++ codefiles/Browser-events/script.js
@@ -1,2 +1,25 @@
+const btn = document.getElementById("btn");
 
+//override
+// btn.onclick = function () {
+//   console.log("hello world");
+// };
+
+// btn.onclick = function () {
+//   console.log("hello world -2");
+// };
+
+//best approch - addEventlistener
+btn.addEventListener("click", function () {
+  console.log("clicked");
+});
+
+//cant be overide
+btn.addEventListener("click", function () {
+  console.log("clicked -1");
+});
+
+btn.addEventListener("click", function () {
+  console.log("clicked -2");
+});
 
